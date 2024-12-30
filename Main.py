74560b1cc0f2e304690981d1a3ac889024e83c76
Rahmawati23-com <rahmawatiibrahim2306@gmail.com>
2024-12-30 18:25:29 +0700

    Main Utama

--- Main.py
+++ Main.py
@@ -1,22 +1,27 @@
 import streamlit as st
 from danasosial import DanaSosial
+from streamlit_option_menu import option_menu
 from dashboard import tampilkan_dashboard
 from pemasukan import pencatatan_pemasukan
 from pengeluaran import pencatatan_pengeluaran
 from persetujuan import persetujuan_pengeluaran
+from tentangkami import tentang_kami
 
 if "dana_sosial" not in st.session_state:
     st.session_state.dana_sosial = DanaSosial()
 
 dana_sosial = st.session_state.dana_sosial
 
-st.sidebar.title("Aplikasi Pengelolaan Dana Sosial")
-menu = st.sidebar.radio("Pilih Menu", [
-    "🏠Dashboard", 
-    "➕Pencatatan Pemasukan", 
-    "💸Pencatatan Pengeluaran", 
-    "✅Persetujuan Pengeluaran"
-])
+with st.sidebar:
+    st.title("Aplikasi Pengelolaan Dana Sosial")
+    menu = option_menu(
+        "Menu Utama",
+        ["Dashboard", "Pencatatan Pemasukan", "Pencatatan Pengeluaran", 
+         "Persetujuan Pengeluaran", "Tentang Kami"],
+        icons=["house", "file-plus", "file-minus", "check-circle", "info-circle"],
+        menu_icon="list",
+        default_index=0,
+    )
 
 if menu == "Dashboard":
     tampilkan_dashboard(dana_sosial)
@@ -26,3 +31,5 @@ elif menu == "Pencatatan Pengeluaran":
     pencatatan_pengeluaran(dana_sosial)
 elif menu == "Persetujuan Pengeluaran":
     persetujuan_pengeluaran(dana_sosial)
+elif menu == "Tentang Kami":
+    tentang_kami() 
